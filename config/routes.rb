84032870a1c2b60b9b6e7ac0be10aeba36a39359
Andrew Phan <n11580267@qut.edu.au>
2024-10-02 13:24:27 +1000

    Create Note Model and Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "notes/new"
+  get "notes/create"
+  get "notes/index"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
